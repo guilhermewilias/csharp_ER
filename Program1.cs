c62a41cbde3e27a15761fd3decc3b82efc0f8aea
Guilherme Wilias <113528431+guilhermewilias@users.noreply.github.com>
2023-11-29 11:31:50 -0300

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,15 @@
+﻿/* 
+ Escreva um programa que exiba os números de 1 a 100 na tela em ordem decrescente
+ */
+
+static void main()
+{
+    Console.WriteLine("Numeros de 1 a 100 em ordem decrescente :");
+
+    for (int i = 100; i >= 1; i--)
+    {
+        Console.WriteLine(i);
+    }
+}
+
+main();
