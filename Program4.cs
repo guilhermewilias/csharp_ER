0b058b0f5d86135f416bcdfb755de6478d47c345
Guilherme Wilias <113528431+guilhermewilias@users.noreply.github.com>
2023-11-29 13:38:39 -0300

    Add files via upload

---
+++ Program4.cs
@@ -0,0 +1,14 @@
+﻿/*
+ Faça um programa que exiba os números de 1 até 2000.
+*/
+
+
+static void main()
+{
+    for (int i = 2000; i >=1; i--)
+    {
+        Console.WriteLine(i);
+    }
+}
+
+main();
