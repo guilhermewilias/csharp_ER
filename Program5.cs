0aa20c725cf95f5196e0aed85028b3e1977b73f4
Guilherme Wilias <113528431+guilhermewilias@users.noreply.github.com>
2023-11-29 13:44:33 -0300

    Add files via upload

---
+++ Program5.cs
@@ -0,0 +1,20 @@
+﻿/*
+ Faça um programa para imprimir uma tabuada. 
+ */
+
+static void main()
+{
+    Console.WriteLine("Digite o numero para ver a tabuada do mesmo : ");
+    int numero = int.Parse(Console.ReadLine()!);
+
+    Console.WriteLine($"\n\nA tabuada do {numero} é :");
+
+    for (int i = 1; i <= 10; i++)
+    {
+        int resultado = numero * i;
+
+        Console.WriteLine($"{numero} x {i} = {resultado}");
+    }
+}
+
+main();
