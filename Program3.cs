5673516b40578f086f810dff160ba5e6092348b2
Guilherme Wilias <113528431+guilhermewilias@users.noreply.github.com>
2023-11-29 13:33:14 -0300

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,15 @@
+﻿/*
+ Faça um programa que gera e escreve os números ímpares dos números lidos entre 100 e 200.
+ */
+
+static void main()
+{
+    Console.WriteLine("Numeros impares lidos entre 100 e 200 : ");
+
+    for (int i = 101; i <= 199; i += 2)
+    {
+        Console.WriteLine(i);
+    } 
+}
+
+main();
