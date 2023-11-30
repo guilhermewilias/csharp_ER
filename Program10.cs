a461733916194c8c9ffcb72f2078e66ab2b1ee60
Guilherme Wilias <113528431+guilhermewilias@users.noreply.github.com>
2023-11-30 11:12:19 -0300

    Add files via upload

---
+++ Program10.cs
@@ -0,0 +1,18 @@
+﻿//Faça um programa que conte de 1 até 100 e a cada múltiplo de 10 exiba uma mensagem: "Múltiplo de 10
+
+static void main()
+{
+    int numero = 0;
+
+    for (int i = 1; i <= 100; i++)
+    {
+        Console.WriteLine(i);
+
+        if (i % 10 == 0)
+        {
+            Console.WriteLine("é multiplo de 10");
+        }
+    }
+}
+
+main();
